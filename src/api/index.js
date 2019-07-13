8c5eaced9e053394ca8047f299d0892357705c2b
Alex Towli <alextowli@hotmail.co.uk>
2019-07-13 14:26:28 +0100

    feat(search): make search request to backend - installs axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,24 @@
 import { Query } from '../utils/validation.utils';
+import axios from 'axios';
+import { config } from '../config';
 
 export function search(query) {
   return new Promise((resolve, reject) => {
     try {
       query = Query.isValid(query);
+      makeRequest(config.API_URL, 'get', { q: query })
+        .then(resolve)
+        .catch(reject);
     } catch (error) {
       reject(error);
     }
+  });
+}
 
-    console.log('perform api search with query: ', query);
-    resolve('query success');
+function makeRequest(url, method, data = null) {
+  return axios({
+    method: method,
+    url: url,
+    params: data
   });
 }

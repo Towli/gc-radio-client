5b32a532857edbff6eb157275a96e984a4cff47d
Alex Towli <alextowli@hotmail.co.uk>
2020-06-27 19:08:26 +0100

    chore(*): envify config and update travis

--- src/config.js
+++ src/config.js
@@ -1,11 +1,14 @@
-// export const config = {
-//   API_URI: 'https://gc-radio-backend.herokuapp.com/search',
-//   WEBSOCKET_URI: 'https://gc-radio-backend.herokuapp.com/',
-//   DOMAIN: 'https://localhost:3000'
-// };
+const config = {};
 
-export const config = {
-  API_URI: 'http://localhost:3001/search',
-  WEBSOCKET_URI: 'http://localhost:3001',
-  DOMAIN: 'https://localhost:3000'
-};
+if (process.env.NODE_ENV === "production") {
+  config.API_URI = "https://gc-radio-backend.herokuapp.com/search";
+  config.WEBSOCKET_URI = "https://gc-radio-backend.herokuapp.com/";
+}
+
+if (process.env.NODE_ENV === "development") {
+  config.API_URI = "http://localhost:3001/search";
+  config.WEBSOCKET_URI = "http://localhost:3001";
+  config.DOMAIN = "https://localhost:3000";
+}
+
+export { config };
